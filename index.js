e0c32a6a88242da5ac763ff21e514eccbfcb43a2
ProgramWithGraham <gaps512@gmail.com>
2024-03-30 22:32:52 -0500

    updated slideshow and started toggle menue

--- index.js
+++ index.js
@@ -31,3 +31,34 @@ document.getElementsByTagName("button")[0].onclick = function () {
     }
   }
 };
+
+function toggleMenu() {
+  const x = document.getElementById("nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+function changeSlides(n) {
+  showSlides((slideIndex += n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let icons = document.getElementsByClassName("ss-icon");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  icons[slideIndex - 1].className += " active";
+}
